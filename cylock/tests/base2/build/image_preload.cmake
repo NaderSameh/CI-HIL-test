e94aac75e763f1078878fa42a7c6380518c39359
nadersameh_ <naders@cypodsolutions.com>
2023-07-12 19:28:47 +0300

    test that out 2

--- cylock/tests/base2/build/image_preload.cmake
+++
@@ -1,5 +0,0 @@
-# Generated file that can be used to preload variant images
-set(CACHED_CONF_FILE C:/ncs2/v1.9.1/nrf/samples/bluetooth/CyCollectorV2I6/cycollector-v2i6/tests/base2/prj.conf CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE  CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION NONE CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON c:\ncs2\v1.9.1\toolchain\opt\bin\python.exe CACHE INTERNAL "NCS child image controlled")
